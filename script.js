6da7d95295c868f9fbef2bcca5d0e72fdd90780c
keenanhammonds <keenan.hammonds@gmail.com>
2019-07-16 15:39:11 -0400

    initial commit/ HTML CSS layout

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('up and running!!')
+
+
+// THE DATA
+// data will be organized into an array of objects
+// each object will have a topic key value pair
+// as well as info or answer key value pair
+// lastly it will have a links portion w links to 
+// w3 schools pages about the topic
+
+// the next card will change the card to the next object in the arr
+
+//The trivia card will start with an image of a topic
+// the trivia card will constantly be switching text content
+// it will default to the topic on the front side
+// after clicking the flip me button...
+// the card will change and will show information about the topic with 
+// a links portion at the bottom
+// next card will go to the next topic
+
+// 
+
+const flipButton = document.querySelector('.flip-button');
+const nextButton = document.querySelector('.next-button');
+const card = document.querySelector('.card');
