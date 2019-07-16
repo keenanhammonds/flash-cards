fbf71b5e0e69abb0217a062fe3c4a72a7d2b7a68
keenanhammonds <keenan.hammonds@gmail.com>
2019-07-16 18:05:12 -0400

    added more data and added a last/previous card button

--- script.js
+++ script.js
@@ -41,11 +41,18 @@ class Data{
 }
 
 let scope = new Data('Scope','JavaScript has function scope: Each function creates a new scope. Scope determines the accessibility (visibility) of these variables. Variables defined inside a function are not accessible (visible) from outside the function.', 'https://www.w3schools.com/js/js_scope.asp')
-let hoisting = new Data('hoisting', 'In JavaScript, a variable can be declared after it has been used. In other words; a variable can be used before it has been declared.', 'https://www.w3schools.com/js/js_hoisting.asp')
-let letKeyword = new Data('let','Before ES2015 JavaScript did not have Block Scope. Variables declared with the let keyword can have Block Scope. Variables declared inside a block {} can not be accessed from outside the block')
-// console.log(scope);
-
-arrOfData.push(scope, hoisting, letKeyword)
+let hoisting = new Data('Hoisting', 'In JavaScript, a variable can be declared after it has been used. In other words; a variable can be used before it has been declared.', 'https://www.w3schools.com/js/js_hoisting.asp')
+let letKeyword = new Data('Let','Before ES2015 JavaScript did not have Block Scope. Variables declared with the let keyword can have Block Scope. Variables declared inside a block {} can not be accessed from outside the block')
+let useStrict = new Data('Use Strict', 'The "use strict" directive was new in ECMAScript version 5. s not a statement, but a literal expression, ignored by earlier versions of JavaScript. The purpose of "use strict" is to indicate that the code should be executed in "strict mode". With strict mode, you can not, for example, use undeclared variables. All modern browsers support "use strict" except Internet Explorer 9 and lower', 'https://www.w3schools.com/js/js_strict.asp')
+let thisKeyword = new Data('This', `The JavaScript this keyword refers to the object it belongs to. It has different values depending on where it is used: In a method, this refers to the owner object.
+Alone, this refers to the global object. In a function, this refers to the global object.In a function, in strict mode, this is undefined.
+In an event, this refers to the element that received the event. Methods like call(), and apply() can refer this to any object.`, 'https://www.w3schools.com/js/js_this.asp')
+let constKeyword = new Data('Const', `Variables defined with const behave like let variables, except they cannot be reassigned. Declaring a variable with const is similar to let when it comes to Block Scope.
+The keyword const is a little misleading. It does NOT define a constant value. It defines a constant reference to a value.
+Because of this, we cannot change constant primitive values, but we can change the properties of constant objects.`, `https://www.w3schools.com/js/js_const.asp`)
+let classKeyword = new Data('Class', `Use the keyword class to create a class, and always add a constructor method. 
+The constructor method is called each time the class object is initialized.`, `https://www.w3schools.com/js/js_classes.asp`) 
+arrOfData.push(scope, hoisting, letKeyword, useStrict,thisKeyword)
 
 let i = 0;
 
